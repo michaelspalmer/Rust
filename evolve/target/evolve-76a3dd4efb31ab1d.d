79b6ded805f8e6dd046568b92377fb7232f76af1
mstephenp <mpalmer1661@gmail.com>
2015-03-19 22:29:14 -0500

    more c

---
+++ evolve/target/evolve-76a3dd4efb31ab1d.d
@@ -0,0 +1,4 @@
+/home/michael/Rust/evolve/target/libevolve-76a3dd4efb31ab1d.rlib: src/lib.rs src/animal/mod.rs src/animal/animal.rs src/functions/mod.rs src/functions/functions.rs src/plant/mod.rs src/plant/plant.rs
+
+/home/michael/Rust/evolve/target/evolve-76a3dd4efb31ab1d.d: src/lib.rs src/animal/mod.rs src/animal/animal.rs src/functions/mod.rs src/functions/functions.rs src/plant/mod.rs src/plant/plant.rs
+
--- some_c/evolve.c
+++ some_c/evolve.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+
+const int WIDTH  = 100;
+const int HEIGHT = 30;
 
 struct animal {
     bool alive;
@@ -7,19 +11,22 @@ struct animal {
     int genes[8];
 };
 
+struct plant {
+	int x, y;
+};
 
 void
 init_animal(struct animal *a)
 {
     int i;
     
-    a->x = 0;
-    a->y = 0;
-    a->energy = 0;
+    a->x = WIDTH / 2;
+    a->y = HEIGHT / 2;
+    a->energy = 1000;
     a->dir = 0;
     a->alive = true;
     
-    for(i = 0; i < 3; i++) {
+    for(i = 0; i < 8; i++) {
         a->genes[i] = i;
     }
 }
@@ -36,28 +43,52 @@ show_animal(struct animal *a)
 void
 move_animal(struct animal *a, int w, int h)
 {
-    a->x = (a->x + (a->dir >= 2 && a->dir < 5) ?  1 :
-                  ((a->dir == 1 || a->dir == 5) ? 0 : 
-                                                 -1) + w) % w;
+    a->x = (a->x + ((a->dir >= 2 && a->dir <  5) ? 1 :
+                    (a->dir == 1 || a->dir == 5) ? 0 : 
+                                                  -1) + w) % w;
                                           
-    a->y += ((a->dir >= 0 && a->dir < 3) ? -1 :
-            ((a->dir >= 4 || a->dir < 7) ?  1 : 
-                                            0) + h) % h;
+    a->y = (a->y + ((a->dir >= 0 && a->dir < 3) ? -1 :
+                    (a->dir >= 4 || a->dir < 7) ?  1 : 
+                                                   0) + h) % h;
     
     a->energy -= 1;
+}
+
+void
+turn_animal(struct animal *a)
+{
+   a->dir += (a->genes[(a->x + a->y) % 8] > 5) ? 1 : -1; 
+   a->dir = abs(a->dir);
+}
+
+void
+eat_animal(struct animal *a, struct plant *p[])
+{
+    int i;
+
+    for (i = 0; i < sizeof(p); i++) {
+	 if (a->x == p[i]->x && a->y == p[i]->y) {
+	      p[i] == 0;
+     	 }
+    }	 
 }
 
 int main() 
 {
     struct animal animal_a;
+    struct plant plants[WIDTH * HEIGHT];
     
     init_animal(&animal_a);
     
     if(animal_a.alive) show_animal(&animal_a);
     
-    move_animal(&animal_a, 100, 30);
+    move_animal(&animal_a, WIDTH, HEIGHT);
     printf("\n");
     
+    if(animal_a.alive) show_animal(&animal_a);
+    printf("\n");
+
+    turn_animal(&animal_a);
     if(animal_a.alive) show_animal(&animal_a);
     
     return 0;
